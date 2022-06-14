33556dbebc79c54d74d6830d003cb6bfc6679c13
Bhaskar <18n81a0560vudaybhaskar@gmail.com>
2022-06-14 11:43:29 +0530

    Domain name -> settings.py

--- Blockchain/settings.py
+++ Blockchain/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mim_*c-n(fe1mn*9thp(=ee!r%-+as5-^g2)c@*js-!40rv03='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','']
+ALLOWED_HOSTS = ['127.0.0.1','decenteralizedblockchainapp.herokuapp.com/']
 
 # Application definition
 
